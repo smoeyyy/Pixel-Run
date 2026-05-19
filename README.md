7d7c48053e56f6e34b9e88aebf77bb20b91a5846
smoeyyy <trwillis@ucsc.edu>
2026-05-18 20:44:37 -0700

    initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+Platformer Game
+
+Spring 2026 CMPM 120
+
+Trinity Willis
+
+Assets from:
---
+++ src/main.js
@@ -0,0 +1,29 @@
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    width: 1440,
+    height: 900,
+    scene: [Platformer]
+}
+
+var cursors;
+const SCALE = 2.0;
+var my = {sprite: {}, text: {}};
+
+const game = new Phaser.Game(config);
